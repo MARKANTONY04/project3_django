8562a108f77d158220c75cbecaaebcc3fd17683b
MARKANTONY04 <markmarsden04@gmail.com>
2025-06-06 22:35:43 +0100

    created menu model

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.TextField(max_length = 300)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    people = models.IntegerField()
+    image = models.ImageField(upload_to='menu/')
+
+
+def __str__(self):
+    return self.name
