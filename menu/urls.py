7f8403eab58a2d43e68ea056c121a9febc81c0b7
MARKANTONY04 <markmarsden04@gmail.com>
2025-06-10 12:56:08 +0100

    added menu list view

---
+++ menu/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+# URL configuration for the menu app
+
+app_name = 'menu'
+
+# The urlpatterns list routes URLs to views in the menu app.
+
+urlpatterns = [
+    path('',views.menu_list, name='menu_list'),
+    path('<slug:slug>' , views.menu_detail, name='menu_detail'),
+]
--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Menu
+# Views for the menu app
+
+def menu_list(request):
+    menu_list = Menu.objects.all()
+
+    return render(request, 'menu/list.html', {'menu_list': menu_list})
+
+
+
+def menu_detail(request, slug):
+    pass
