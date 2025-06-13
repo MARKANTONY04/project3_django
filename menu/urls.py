22aa2839798af0ac9a228199f2d48f58992fded4
MARKANTONY04 <markmarsden04@gmail.com>
2025-06-13 17:51:12 +0100

    removed extra base.html and fixed bugs in the correct base.html

--- menu/urls.py
+++ menu/urls.py
@@ -8,6 +8,6 @@ app_name = 'menu'
 # The urlpatterns list routes URLs to views in the menu app.
 
 urlpatterns = [
-    path('',views.menu_list, name='menu_list'),
+    path('', views.menu_list, name='menu_list'),
     path('<slug:slug>' , views.menu_detail, name='menu_detail'),
 ]
