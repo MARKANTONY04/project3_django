44d0f8a588176e37e07e9c8d1ebb87c7ea075137
MARKANTONY04 <markmarsden04@gmail.com>
2025-06-16 19:07:59 +0100

    migrated reservation model

--- reservation/models.py
+++ reservation/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 
-class Reservation(model.Model):
+class Reservation(models.Model):
     name = models.CharField(max_length=50)
-    phone_number = models.IntegerField(max_length=15)
+    phone_number = models.IntegerField()
     email = models.EmailField()
     date = models.DateField()
     time = models.TimeField()
     number_of_guests = models.PositiveIntegerField()
 
     def __str__(self):
-        return f"Reservation for {self.name} on {self.date} at {self.time}"
+        return f"Booking confirmed. Reservation for {self.name} on {self.date} at {self.time}. We look forward to seeing you."
