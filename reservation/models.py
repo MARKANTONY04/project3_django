6b04b04cd0ab0bb11c16b87c923b1727a03a1c89
MARKANTONY04 <markmarsden04@gmail.com>
2025-06-16 19:01:46 +0100

    created Reservation class in models.py

--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Reservation(model.Model):
+    name = models.CharField(max_length=50)
+    phone_number = models.IntegerField(max_length=15)
+    email = models.EmailField()
+    date = models.DateField()
+    time = models.TimeField()
+    number_of_guests = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"Reservation for {self.name} on {self.date} at {self.time}"
