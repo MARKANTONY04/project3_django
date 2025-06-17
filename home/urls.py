55f3c551d1e3157c7b08ae61a03fab6ced66a609
MARKANTONY04 <markmarsden04@gmail.com>
2025-06-17 10:38:52 +0100

    created urls.py with code for home app

---
+++ home/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+# URL configuration for the home app
+
+app_name = 'home'
+
+# The urlpatterns list routes URLs to views in the home app.
+
+urlpatterns = [
+    path('', views.home_view, name='home_view'),
