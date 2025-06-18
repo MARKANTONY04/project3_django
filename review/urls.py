927c492b880416081faf4cf295ca8bbf69ceda61
MARKANTONY04 <markmarsden04@gmail.com>
2025-06-18 21:17:16 +0100

    added post_review and register functions for views.py

--- review/urls.py
+++ review/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
 
@@ -8,4 +9,7 @@ app_name = 'review'
 urlpatterns = [
     path('submit/', views.submit_review, name='submit_review'),
     path('', views.reviews_list, name='reviews_list'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
+
--- review/views.py
+++ review/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from .models import Review
 from .forms import ReviewForm
@@ -19,3 +21,28 @@ def submit_review(request):
 def reviews_list(request):
     reviews = Review.objects.all().order_by('-created_at')
     return render(request, 'review/reviews_list.html', {'reviews': reviews})
+
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+@login_required
+def post_review(request):
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.user = request.user
+            review.save()
+            return redirect('reviews_list')
+    else:
+        form = ReviewForm()
+    return render(request, 'review/post_review.html', {'form': form})
