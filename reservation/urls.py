2f93ece3a7793c2e5e4efef8b41f7848641b32f1
MARKANTONY04 <markmarsden04@gmail.com>
2025-11-20 11:39:05 +0000

    updates urls.py in reservation to cater for new view functionality

--- reservation/urls.py
+++ reservation/urls.py
@@ -1,5 +1,26 @@
+# from django.urls import path
+# from . import views
+
+# URL configuration for the menu app
+
+# app_name = 'reservation'
+
+# The urlpatterns list routes URLs to views in the menu app.
+
+# urlpatterns = [
+#     path('', views.reservation_view, name='reservation_view'),
+#     path("book/", create_reservation, name="create_reservation"),
+#     
+# ]
+
+
 from django.urls import path
-from . import views
+from .views import (
+    create_reservation,
+    reservation_list,
+    edit_reservation,
+    delete_reservation,
+)
 
 # URL configuration for the menu app
 
@@ -8,5 +29,8 @@ app_name = 'reservation'
 # The urlpatterns list routes URLs to views in the menu app.
 
 urlpatterns = [
-    path('', views.reservation_view, name='reservation_view'),
+    path("book/", create_reservation, name="create_reservation"),
+    path("my-reservations/", reservation_list, name="reservation_list"),
+    path("edit/<int:pk>/", edit_reservation, name="edit_reservation"),
+    path("delete/<int:pk>/", delete_reservation, name="delete_reservation"),
 ]
