80dad679b6da86539f0222b7143e53e3cb02d124
MARKANTONY04 <markmarsden04@gmail.com>
2025-11-20 15:15:37 +0000

    updated reservation urls.py to fit with new models and views

--- reservation/urls.py
+++ reservation/urls.py
@@ -1,36 +1,12 @@
-# from django.urls import path
-# from . import views
-
-# URL configuration for the menu app
-
-# app_name = 'reservation'
-
-# The urlpatterns list routes URLs to views in the menu app.
-
-# urlpatterns = [
-#     path('', views.reservation_view, name='reservation_view'),
-#     path("book/", create_reservation, name="create_reservation"),
-#     
-# ]
-
-
 from django.urls import path
-from .views import (
-    create_reservation,
-    reservation_list,
-    edit_reservation,
-    delete_reservation,
-)
-
-# URL configuration for the menu app
+from . import views
 
 app_name = 'reservation'
 
-# The urlpatterns list routes URLs to views in the menu app.
-
 urlpatterns = [
-    path("book/", create_reservation, name="create_reservation"),
-    path("my-reservations/", reservation_list, name="reservation_list"),
-    path("edit/<int:pk>/", edit_reservation, name="edit_reservation"),
-    path("delete/<int:pk>/", delete_reservation, name="delete_reservation"),
+    path("book/", views.create_reservation, name="create_reservation"),
+    path("my-reservations/", views.reservation_list, name="reservation_list"),
+    path("edit/<int:pk>/", views.edit_reservation, name="edit_reservation"),
+    path("delete/<int:pk>/", views.delete_reservation, name="delete_reservation"),
+    path("check-availability/", views.check_availability, name="check_availability"),
 ]
